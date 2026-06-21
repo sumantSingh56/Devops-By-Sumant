f823cb6c0bad0dd18f43f273878b7cb2c6652807
Sumant Singh <sumantsinghbhumihar5656@gmail.com>
2026-06-21 15:05:36 +0530

    Update for_loops.sh

--- shell_scripting/for_loops.sh
+++ shell_scripting/for_loops.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
 
+<<sks
+Thish shell is explanation of functions
+sks
+
+#user_create
+
+function create_user {
+
+read -p "enter the user name " username
+
+sudo useradd -m "$username"
+
+echo "user created successfuly"
+
+}
+
+create_user
+
+<<sks
+
+#delete_user
+
+function delete_user {
+
+read -p "enter the delete user_name " username
+
+sudo userdel -r $username
