e1e7448495b0e51e1eca3a24b05278b4bd6445bf
Sumant Singh <sumantsinghbhumihar5656@gmail.com>
2026-06-21 15:08:39 +0530

    Update for_loops.sh

--- shell_scripting/for_loops.sh
+++ shell_scripting/for_loops.sh
@@ -1,29 +1,35 @@
-#!/bin/bash
-
 <<sks
-Thish shell is explanation of functions
+
+loops: Thish condition is repeat and repeat and repeat based conditions
+(kisi chij ko vaar duhrane ke lopps ka use kiya jata hai)
+example: kisi chij ko 10 vaar duhrana hai .
+starting pouint = 01
+ending point = 10
+increment/decrement = + / -
 sks
 
-#user_create
+for (( num=1 ; num<=10 ; num++))
+do
+        echo "$num"
+done
+
+for (( i=1 ; i<=5 ; i++ ))
+do
+        read -p " enter the user_name: " username
+
+        sudo useradd -m $username
+
+        echo "$username user adding is completed"
+done
 
 function create_user {
 
-read -p "enter the user name " username
-
-sudo useradd -m "$username"
-
-echo "user created successfuly"
-
+        read  -p " enter the user_name: " username
+        sudo useradd -m $username
+        echo "ucer add complete"
 }
 
-create_user
-
-<<sks
-
-#delete_user
-
-function delete_user {
-
-read -p "enter the delete user_name " username
-
-sudo userdel -r $username
+for (( i=1 ; i<=5 ; i++ ))
+do
+        create_user
+done
