f94c32dc3f3c3126da9d3418a2b904b03f7e8b16
Sumant Singh <sumantsinghbhumihar5656@gmail.com>
2026-06-21 15:07:37 +0530

    Update if_else.sh

--- shell_scripting/if_else.sh
+++ shell_scripting/if_else.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+<<sks
+Thish shell is checked by user deleted
+sks
+
+read -p "Enter the username cheked by deleted_user " username
+
+count=$(cat /etc/passwd | grep -i $username | wc | awk '{print $1}')
+
+if [ $count == 0 ];
+then
+
+        echo "user not exist"
+
+else
+        echo "user exist"
+fi
